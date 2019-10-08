4dbbf049d07c9399ae58b60d8a2c5a1fd5cd7805
mokoenatshepo28398084 <47389808+mokoenatshepo28398084@users.noreply.github.com>
2019-10-08 10:27:15 +0200

    Update Client.py

--- LAB3/Client.py
+++ LAB3/Client.py
@@ -1,17 +1,13 @@
+#!/usr/bin/env python3
+
 import socket
 
-HOST = '127.0.0.1' # Standard loopback interface address (localhost)
-PORT = 999	# Port to listen on
+HOST = '127.0.0.1' # The server's hostname or IP address
+PORT = 9999	# The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.bind((HOST, PORT))
-	s.listen()
-	conn, addr = s.accept()
-	with conn:
-		print('Server is connected with address', addr)
-		while True:
-			data = conn.recv(2048) #default byte stream size
-		if not data:
-			break
-		conn.sendall(data)
-© 2019 GitHub, Inc.
+	s.connect((HOST. PORT))
+	s.sendall(b'Hello, Server')
+	data = s.recv(1024)
+
+print('Recaived', repr(date))
